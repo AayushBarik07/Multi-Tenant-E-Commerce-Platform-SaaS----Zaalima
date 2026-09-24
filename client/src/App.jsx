7bb6578a4fac723329f5376ad12c91d17dda784b
Aayush <superbsup45@gmail.com>
2026-09-24 17:27:57 +0530

    feat(ui): hide shopping cart navigation icon for admin accounts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import Home from './pages/public/Home';
 import Storefront from './pages/public/Storefront';
 import ProductDetails from './pages/public/ProductDetails';
 import Checkout from './pages/public/Checkout';
+import Success from './pages/public/Success';
 import BecomeVendor from './pages/public/BecomeVendor';
 import CartDrawer from './components/public/CartDrawer';
 import { toggleCart } from './redux/slices/cartSlice';
@@ -128,19 +129,22 @@ function App() {
                 </Link>
               )}
 
-              <button 
-                onClick={() => dispatch(toggleCart())}
-                className="text-gray-600 hover:text-gray-900 relative"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-                </svg>
-                {cartItemsCount > 0 && (
-                  <span className="absolute -top-2 -right-2 bg-indigo-600 text-white text-xs font-bold w-5 h-5 rounded-full flex items-center justify-center">
-                    {cartItemsCount}
-                  </span>
-                )}
-              </button>
+              {/* Hide Cart for Super Admins */}
+              {(!dbUser || dbUser.role !== 'SUPER_ADMIN') && (
+                <button 
+                  onClick={() => dispatch(toggleCart())}
+                  className="text-gray-600 hover:text-gray-900 relative"
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                  </svg>
+                  {cartItemsCount > 0 && (
+                    <span className="absolute -top-2 -right-2 bg-indigo-600 text-white text-xs font-bold w-5 h-5 rounded-full flex items-center justify-center">
+                      {cartItemsCount}
+                    </span>
+                  )}
+                </button>
+              )}
               
               <SignedIn>
                 <div className="flex items-center space-x-4 border-l border-gray-200 pl-4 ml-2">
@@ -179,7 +183,8 @@ function App() {
               <Route path="/store/:storeId" element={<Storefront />} />
               <Route path="/product/:id" element={<ProductDetails />} />
               <Route path="/become-vendor" element={<BecomeVendor />} />
-              <Route path="/checkout" element={
+              <Route path="/success/:transactionId" element={<Success />} />
+                <Route path="/checkout" element={
                 <SignedIn>
                   <Checkout />
                 </SignedIn>
